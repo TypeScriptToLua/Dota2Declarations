efc25a3b5eff2887538eb655bd0a73685135bdc1
DoctorGester <doctorgester@gmail.com>
2020-05-10 16:08:07 +0200

    Most data is now passed as JSON stream

--- json.d.ts
+++ json.d.ts
@@ -1,5 +1,5 @@
 /** @noSelf */
 declare namespace json {
     function decode(input: string): object;
-    function encode(input: object): string;
+    function encode(input: object, params: object): string;
 }
