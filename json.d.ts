da90b089e9a05b12c98559d6dbc918a49ba0931d
RobinCode <a348730140@163.com>
2020-05-27 03:23:26 +0800

    Update json.d.ts (#13)

--- json.d.ts
+++ json.d.ts
@@ -1,5 +1,11 @@
 /** @noSelf */
 declare namespace json {
     function decode(input: string): object;
-    function encode(input: object, params: object): string;
+    function encode(
+        input: object, 
+        params?: {
+            indent?: boolean,
+            level?: number,
+        }
+    ): string;
 }
