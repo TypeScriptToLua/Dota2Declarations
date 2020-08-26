68d59bd8713b09eed7cb8db4dd91c615f79e240c
RobinCode <a348730140@163.com>
2020-08-26 17:22:13 +0800

    update

--- dota-std.d.ts
+++ dota-std.d.ts
@@ -1,3 +1,3 @@
 declare function print(this: void, ...messages: any[]): void;
 declare function require(this: void, module: string): any;
-declare function DeepPrintTable(this: void, tbl: object): any;
+declare function DeepPrintTable(this: void, tbl: object, prefix?: string): any;
